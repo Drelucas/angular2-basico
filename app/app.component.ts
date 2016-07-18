d69cb9c6856d903b937060078a6e056de4806cb7
drelucastest <drelucastest@gmail.com>
2016-07-18 17:30:29 +0000

    ngModel

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
 	template: `
             <h1>{{ title }}</h1>
             <input type= "text" value ="{{ title }}"/>
+            <input type= "text" [(ngModel)]="title" />
             `
 })
 
