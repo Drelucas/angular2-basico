52b48b1904f1423c7bd0b17eb2689ab4fc77ee19
drelucastest <drelucastest@gmail.com>
2016-07-18 17:12:43 +0000

    add commands

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: `
+            <h1>{{ title }}</h1>
+            <input type= "text" value ="{{ title }}"/>
+            `
+})
+
+export class AppComponent{
+  title= "André Lucas"
+}
